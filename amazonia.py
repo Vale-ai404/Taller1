ea0f5b6b5acf165612d75925c1fa313d6de31a4a
vale <valeryveloza88@gmail.com>
2026-04-28 18:48:19 -0500

    Agregar mi animal

--- amazonia.py
+++ amazonia.py
@@ -3,6 +3,7 @@
 
 animales = [
     {"nombre": "Jaguar", "dato": "Es el felino mas grande de America."},
+    {"nombre": "Delfin rosado", "dato": "Vive en los rios de la Amazonia."},
 ]
 
 print("=== Animales de la Amazonia ===")
